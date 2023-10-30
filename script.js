477812b554c04de34b299fcf8c842539260a124d
ShivamSingh208 <shivampal04240@gmail.com>
2023-10-31 02:09:48 +0530

    song name are update

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ let songs = [
     {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
     {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
     {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Farq Hai - Suzonn New ", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Blasterjaxx - Legion", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
